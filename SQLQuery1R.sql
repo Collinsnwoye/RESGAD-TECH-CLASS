025034f7060b5d812768234d4ac799d92b2150e9
Collinsnwoye <160544205+Collinsnwoye@users.noreply.github.com>
2025-07-20 01:34:53 +0100

    Update SQLQuery1R.sql

--- SQLQuery1R.sql
+++ SQLQuery1R.sql
@@ -1,12 +1,3 @@
---DECLARE @start_time DATETIME = GETDATE()
---DECLARE @counter INT = 1
-
---WHILE DATEDIFF(SECOND, @start_time, GETDATE()) < 1
---BEGIN
---    PRINT CAST(@counter AS VARCHAR)
---    SET @counter = @counter + 1
---END
-
 DECLARE @start_time DATETIME = GETDATE()
 DECLARE @date DATETIME = GETDATE()
 
