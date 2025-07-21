1e7031430a900c4dea7cc09a7d6506e7b5790caa
Collinsnwoye <160544205+Collinsnwoye@users.noreply.github.com>
2025-07-21 13:33:25 +0100

    Update SQLQuery_Streaming.sql

--- SQLQuery1B.sql
+++ SQLQuery1B.sql
@@ -1,8 +1,17 @@
 DECLARE @start_time DATETIME = GETDATE()
 DECLARE @counter INT = 1
 
-WHILE DATEDIFF(SECOND, @start_time, GETDATE()) < 1
+WHILE DATEDIFF(SECOND, @start_time, GETDATE()) < 10
 BEGIN
     PRINT CAST(@counter AS VARCHAR)
     SET @counter = @counter + 1
+END
+
+DECLARE @start_time DATETIME = GETDATE()
+DECLARE @date DATETIME = GETDATE()
+
+WHILE DATEDIFF(SECOND, @start_time, GETDATE()) < 10
+BEGIN
+    PRINT CAST(@date AS VARCHAR)
+    SET @date = DATEADD(DAY, 1, @date)
 END
